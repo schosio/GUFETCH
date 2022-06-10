58a1ce65a8a1409a3744e31e83572cf1134ac92a
Haneesh Jindal <94955130+schosio@users.noreply.github.com>
2022-06-10 15:16:07 +0530

    Update gufetch.sh

--- gufetch.sh
+++ gufetch.sh
@@ -1,1 +1,10 @@
-ss
+description=$(zenity --forms --title="Download of the SRA files" --text="Give SRR IDs (sep by ',')" --add-entry="SRR IDS 
+(sep by ',')
+[SRR12345, SRR23456, SRR34567]")
+
+[[ $? != 0 ]] && exit 1
+snames=$(echo $description | cut -d'|' -f1)
+
+if [ "$?" -eq "0" ]; then
+  data_download.sh $snames
+fi
