4b17a8b455618fccbe58efab3497f317ea1cbabc
Haneesh Jindal <94955130+schosio@users.noreply.github.com>
2022-06-10 15:36:54 +0530

    Update data_download.sh

--- data_download.sh
+++ data_download.sh
@@ -11,10 +11,11 @@ srr_acc=$1
 # for checking space
 touch temp_space.txt
 temp=0
-while read -r line; do
-                        space=$(vdb-dump --info $line | grep size | awk -F ":" '{print $2}'| tr -d ',')
-                        temp=$(( $temp + $space ))
-                    done < ${srr_acc}
+while read -r line
+  do
+     space=$(vdb-dump --info $line | grep size | awk -F ":" '{print $2}'| tr -d ',')
+     temp=$(( $temp + $space ))
+  done < ${srr_acc}
 # spcae in GB
 new=$((temp*10))
 new=$(echo $new | awk '{print $1/1024/1024/1024}')
