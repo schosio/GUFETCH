16798d7429f4d1a604fdf6d1bc9e651f03e70ba6
Haneesh Jindal <94955130+schosio@users.noreply.github.com>
2022-06-10 16:00:50 +0530

    Update gufetch.sh

--- gufetch.sh
+++ gufetch.sh
@@ -7,6 +7,17 @@ snames=$(echo $description | cut -d'|' -f1)
 
 echo $snames | tr "," "\n" >srr_acc.txt
 
+# check wheather SRA toolkit is installed or not
+
+which vdb-config >/dev/null 2>&1
+if [[ $? -ne 0 ]]; then
+mkdir -p /opt/application
+cd /opt/application
+wget http://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/2.4.1/sratoolkit.2.4.1-ubuntu64.tar.gz
+tar xzvf sratoolkit.2.4.1-ubuntu64.tar.gz
+export PATH=$PATH:/opt/application/sratoolkit.2.4.1-ubuntu64/bin
+fi
+
 if [ "$?" -eq "0" ]; then
   bash $PWD/data_download.sh 
 fi
