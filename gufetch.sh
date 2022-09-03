2fd50e896f380000e2fad31d7baa641f30df29e9
Haneesh Jindal <94955130+schosio@users.noreply.github.com>
2022-09-03 14:41:01 +0530

    Update gufetch.sh

--- gufetch.sh
+++ gufetch.sh
@@ -17,11 +17,11 @@ SRR=$(zenity --file-selection --filename  \
 
 which vdb-config >/dev/null 2>&1
 if [[ $? -ne 0 ]]; then
-  mkdir -p ~/application
+  mkdir -p $HOME/application
   cd ~/application
   wget http://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/2.4.1/sratoolkit.2.4.1-ubuntu64.tar.gz
   tar xzvf sratoolkit.2.4.1-ubuntu64.tar.gz
-  export PATH=$PATH:~/application/sratoolkit.2.4.1-ubuntu64/bin
+  export PATH="$HOME/application/sratoolkit.2.4.1-ubuntu64/bin:$PATH"
   cd -
 fi
 
